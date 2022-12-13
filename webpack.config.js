587a6ee4d2117acca8f5a588cf4224b3130be098
Conor Vernon <conor.vernon@port.ac.uk>
2022-12-13 14:41:13 +0000

    Changed dist to docs

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/js/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
   },
   performance: {
     maxEntrypointSize: 4096000,
